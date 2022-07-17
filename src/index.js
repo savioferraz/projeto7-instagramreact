35bb26967d6ebc45a14b5b41d0723c097b4a3b42
savioferraz <savio.ferraz88@gmail.com>
2022-07-16 23:13:43 -0300

    index App criado

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            olá mundo
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
